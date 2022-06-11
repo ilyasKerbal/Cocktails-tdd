90f863ec9107016d5bf76614690875b56aed90df
ILYAS KERBAL <kerbalsc@gmail.com>
2022-06-11 16:25:54 +0100

    Question tests use hamcrest

--- app/src/test/java/io/github/ilyaskerbal/cocktails_quizz/QuestionUnitTests.kt
+++ app/src/test/java/io/github/ilyaskerbal/cocktails_quizz/QuestionUnitTests.kt
@@ -1,6 +1,10 @@
 package io.github.ilyaskerbal.cocktails_quizz
 
 import io.github.ilyaskerbal.cocktails_quizz.game.model.Question
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -20,28 +24,28 @@ class QuestionUnitTests {
 
     @Test
     fun question_creatingQuestion_shouldNotHaveAnsweredOption() {
-        Assert.assertNull(question.answeredOption)
+        assertThat(question.answeredOption, nullValue())
     }
 
     @Test
     fun question_whenAnswered_shouldHaveAnsweredOption() {
         question.answer(INCORRECT_ANSWER)
 
-        Assert.assertEquals(question.answeredOption, INCORRECT_ANSWER)
+        assertThat(question.answeredOption, equalTo(INCORRECT_ANSWER))
     }
 
     @Test
     fun question_whenAnsweredCorrectly_shouldReturnTrue() {
         val result = question.answer(CORRECT_ANSWER)
 
-        Assert.assertTrue(result)
+        assertThat(result, equalTo(true))
     }
 
     @Test
     fun question_whenAnsweredIncorrectly_shouldReturnFalse() {
         val result = question.answer(INCORRECT_ANSWER)
 
-        Assert.assertFalse(result)
+        assertThat(result, equalTo(false))
     }
 
     @Test(expected = IllegalArgumentException::class)
@@ -53,6 +57,6 @@ class QuestionUnitTests {
     fun getOptions_unsorted_shouldReturnSortedOptions_givenPredicate() {
         val options = question.getOptions { it.reversed() }
         val expected = listOf<String>(INCORRECT_ANSWER, CORRECT_ANSWER)
-        Assert.assertEquals(expected, options)
+        assertThat(options, equalTo(expected))
     }
 }
